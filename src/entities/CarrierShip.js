6d4e39bab50da866e538fb0ddb308b1baed076a0
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-04-29 12:55:07 +0300

    fix: remove lagging& lint pass

--- src/entities/CarrierShip.js
+++ src/entities/CarrierShip.js
@@ -1,8 +1,9 @@
-import Entity from "../entities/Entity";
+import Entity from './Entity';
+
 export default class CarrierShip extends Entity {
-    constructor(scene, x, y) {
-        super(scene, x, y, 'sprEnemy2', 'CarrierShip');
-        this.body.velocity.y = Phaser.Math.Between(50, 100);
-        this.play('sprEnemy2');
-    }
+  constructor(scene, x, y) {
+    super(scene, x, y, 'sprEnemy2', 'CarrierShip');
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
+    this.play('sprEnemy2');
+  }
 }
