4b4fef9987999103a2f5cbdd3187c3cb65d41c6f
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-04-29 10:22:58 +0300

    feat: load assets in main menu

--- src/scenes/SceneMainMenu.js
+++ src/scenes/SceneMainMenu.js
@@ -3,6 +3,18 @@ export default class SceneMainMenu extends Phaser.Scene {
         super({ key: "SceneMainMenu" });
     }
 
+    preload() {
+        // images
+        this.load.image("sprBtnPlay", "assets/sprBtnPlay.png");
+        this.load.image("sprBtnPlayHover", "assets/sprBtnPlayHover.png");
+        this.load.image("sprBtnPlayDown", "assets/sprBtnPlayDown.png");
+        this.load.image("sprBtnRestart", "assets/sprBtnRestart.png");
+        this.load.image("sprBtnRestartHover", "assets/sprBtnRestartHover.png");
+        this.load.image("sprBtnRestartDown", "assets/sprBtnRestartDown.png");
+        // audio
+        this.load.audio("sndBtnOver", "assets/sndBtnOver.wav");
+        this.load.audio("sndBtnDown", "assets/sndBtnDown.wav");
+    }
     create() {
         this.scene.start("SceneMain");
     }
