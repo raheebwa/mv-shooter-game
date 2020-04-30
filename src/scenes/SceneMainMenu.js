ada9f1f75724c70bf662a2b155dfdd5f81dd8a2c
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-04-30 05:56:51 +0300

    feat: added main menu

--- src/scenes/SceneMainMenu.js
+++ src/scenes/SceneMainMenu.js
@@ -1,23 +1,89 @@
 /* eslint-disable no-undef */
+import 'phaser';
+import ScrollingBackground from "../entities/ScrollingBackground";
 export default class SceneMainMenu extends Phaser.Scene {
-  constructor() {
-    super({ key: 'SceneMainMenu' });
-  }
+    constructor() {
+        super({ key: 'SceneMainMenu' });
+    }
 
-  preload() {
-    // images
-    this.load.image('sprBtnPlay', 'assets/sprBtnPlay.png');
-    this.load.image('sprBtnPlayHover', 'assets/sprBtnPlayHover.png');
-    this.load.image('sprBtnPlayDown', 'assets/sprBtnPlayDown.png');
-    this.load.image('sprBtnRestart', 'assets/sprBtnRestart.png');
-    this.load.image('sprBtnRestartHover', 'assets/sprBtnRestartHover.png');
-    this.load.image('sprBtnRestartDown', 'assets/sprBtnRestartDown.png');
-    // audio
-    this.load.audio('sndBtnOver', 'assets/sndBtnOver.wav');
-    this.load.audio('sndBtnDown', 'assets/sndBtnDown.wav');
-  }
+    preload() {
+        // images
+        this.load.image("sprBg0", "assets/sprBg0.png");
+        this.load.image("sprBg1", "assets/sprBg1.png");
 
-  create() {
-    this.scene.start('SceneMain');
-  }
+        this.load.image('sprBtnPlay', 'assets/sprBtnPlay.png');
+        this.load.image('sprBtnPlayHover', 'assets/sprBtnPlayHover.png');
+        this.load.image('sprBtnPlayDown', 'assets/sprBtnPlayDown.png');
+        this.load.image('sprBtnRestart', 'assets/sprBtnRestart.png');
+        this.load.image('sprBtnRestartHover', 'assets/sprBtnRestartHover.png');
+        this.load.image('sprBtnRestartDown', 'assets/sprBtnRestartDown.png');
+        // audio
+        this.load.audio('sndBtnOver', 'assets/sndBtnOver.wav');
+        this.load.audio('sndBtnDown', 'assets/sndBtnDown.wav');
+    }
+
+    create() {
+        this.sfx = {
+            btnOver: this.sound.add("sndBtnOver"),
+            btnDown: this.sound.add("sndBtnDown")
+        };
+
+        this.btnPlay = this.add.sprite(
+            this.game.config.width * 0.5,
+            this.game.config.height * 0.5,
+            "sprBtnPlay"
+        );
+
+        this.btnPlay.setInteractive();
+
+        this.btnPlay.on("pointerover", function() {
+            this.btnPlay.setTexture("sprBtnPlayHover"); // set the button texture to sprBtnPlayHover
+            this.sfx.btnOver.play(); // play the button over sound
+        }, this);
+
+        this.btnPlay.on("pointerout", function() {
+            this.setTexture("sprBtnPlay");
+        });
+
+        this.btnPlay.on("pointerdown", function() {
+            this.btnPlay.setTexture("sprBtnPlayDown");
+            this.sfx.btnDown.play();
+        }, this);
+
+        this.btnPlay.on("pointerup", function() {
+            this.btnPlay.setTexture("sprBtnPlay");
+        }, this);
+
+        this.title = this.add.text(this.game.config.width * 0.5, 128, "RAMAR'S SHOOTER GAME", {
+            fontFamily: 'monospace',
+            fontSize: 35,
+            fontStyle: 'bold',
+            color: '#ffffff',
+            align: 'center'
+        });
+
+        this.title.setOrigin(0.5);
+
+
+        this.btnPlay.on("pointerup", function() {
+            this.btnPlay.setTexture("sprBtnPlay");
+            this.scene.start("SceneMain");
+        }, this);
+
+
+        this.backgrounds = [];
+        for (var i = 0; i < 5; i++) {
+            var keys = ["sprBg0", "sprBg1"];
+            var key = keys[Phaser.Math.Between(0, keys.length - 1)];
+            var bg = new ScrollingBackground(this, key, i * 10);
+            this.backgrounds.push(bg);
+        }
+    }
+
+
+    update() {
+        for (var i = 0; i < this.backgrounds.length; i++) {
+            this.backgrounds[i].update();
+        }
+    }
 }
