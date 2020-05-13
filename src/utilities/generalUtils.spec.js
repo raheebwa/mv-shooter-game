f1118a6f2fb3c024917ede8a0c0438cb43357c53
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-05-13 12:57:10 +0300

    lint:fix

--- src/utilities/generalUtils.spec.js
+++ src/utilities/generalUtils.spec.js
@@ -1,61 +1,61 @@
-import { randomNumber, dynamicsort } from "./generalUtils";
+import { randomNumber, dynamicsort } from './generalUtils';
 
 const unsorted = [{
-        name: "rama",
-        score: 40,
-    },
-    {
-        name: "bad",
-        score: 20,
-    },
+  name: 'rama',
+  score: 40,
+},
+{
+  name: 'bad',
+  score: 20,
+},
 
-    {
-        name: "badr",
-        score: 200,
-    },
+{
+  name: 'badr',
+  score: 200,
+},
 ];
 
 const sortedScoreAsc = [{
-        name: "bad",
-        score: 20,
-    },
-    {
-        name: "rama",
-        score: 40,
-    },
+  name: 'bad',
+  score: 20,
+},
+{
+  name: 'rama',
+  score: 40,
+},
 
-    {
-        name: "badr",
-        score: 200,
-    },
+{
+  name: 'badr',
+  score: 200,
+},
 ];
 
 const sortedScoreDesc = [{
-        name: "badr",
-        score: 200,
-    },
-    {
-        name: "rama",
-        score: 40,
-    },
-    {
-        name: "bad",
-        score: 20,
-    },
+  name: 'badr',
+  score: 200,
+},
+{
+  name: 'rama',
+  score: 40,
+},
+{
+  name: 'bad',
+  score: 20,
+},
 ];
 
-it("should return a  random number", () => {
-    expect(typeof randomNumber()).toBe("number");
+it('should return a  random number', () => {
+  expect(typeof randomNumber()).toBe('number');
 });
 
-it("should sort array descending by score", () => {
-    let sortedToD = unsorted.sort(dynamicsort("score", "desc"));
+it('should sort array descending by score', () => {
+  const sortedToD = unsorted.sort(dynamicsort('score', 'desc'));
 
-    expect(sortedToD).toEqual(sortedScoreDesc);
+  expect(sortedToD).toEqual(sortedScoreDesc);
 });
 
-it("should sort array ascending by score", () => {
-    let sortedToA = unsorted.sort(dynamicsort("score", "asc"));
+it('should sort array ascending by score', () => {
+  const sortedToA = unsorted.sort(dynamicsort('score', 'asc'));
 
-    expect(sortedToA).toEqual(sortedScoreAsc);
+  expect(sortedToA).toEqual(sortedScoreAsc);
 });
