0f20d5e92e61908d70c08be12862da1304115872
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-05-13 12:56:01 +0300

    test: helper functions

---
+++ src/utilities/generalUtils.spec.js
@@ -0,0 +1,61 @@
+import { randomNumber, dynamicsort } from "./generalUtils";
+
+const unsorted = [{
+        name: "rama",
+        score: 40,
+    },
+    {
+        name: "bad",
+        score: 20,
+    },
+
+    {
+        name: "badr",
+        score: 200,
+    },
+];
+
+const sortedScoreAsc = [{
+        name: "bad",
+        score: 20,
+    },
+    {
+        name: "rama",
+        score: 40,
+    },
+
+    {
+        name: "badr",
+        score: 200,
+    },
+];
+
+const sortedScoreDesc = [{
+        name: "badr",
+        score: 200,
+    },
+    {
+        name: "rama",
+        score: 40,
+    },
+    {
+        name: "bad",
+        score: 20,
+    },
+];
+
+it("should return a  random number", () => {
+    expect(typeof randomNumber()).toBe("number");
+});
+
+it("should sort array descending by score", () => {
+    let sortedToD = unsorted.sort(dynamicsort("score", "desc"));
+
+    expect(sortedToD).toEqual(sortedScoreDesc);
+});
+
+it("should sort array ascending by score", () => {
+    let sortedToA = unsorted.sort(dynamicsort("score", "asc"));
+
+    expect(sortedToA).toEqual(sortedScoreAsc);
+});
